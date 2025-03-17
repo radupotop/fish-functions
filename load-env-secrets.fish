1eb2a9433c36581558064915141d9e42162a367d
Radu Potop <radu@wooptoo.com>
2025-03-17 23:11:31 +0000

    Added usage to load-env-secrets

--- load-env-secrets.fish
+++ load-env-secrets.fish
@@ -1,3 +1,5 @@
+# Usage: load-env-secrets env_var1 env_var2
+
 function load-env-secrets --description "Load ENV secrets from pass store"
     for key in $argv
         if set -l password (pass show env/$key)
